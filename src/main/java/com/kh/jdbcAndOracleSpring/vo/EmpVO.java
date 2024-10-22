2e158b54a2b8da94f651addade07252cfc6765cc
networkSorcerer <gigattfyhig@gmail.com>
2024-10-22 15:39:34 +0900

    jdbcSpring

---
+++ src/main/java/com/kh/jdbcAndOracleSpring/vo/EmpVO.java
@@ -0,0 +1,22 @@
+package com.kh.jdbcAndOracleSpring.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmpVO {
+    private int empNO;
+    private String name;
+    private String job;
+    private int mgr;
+    private Date date;
+    private BigDecimal sal;
+    private BigDecimal comm;
+    private int deptNO;
+}
